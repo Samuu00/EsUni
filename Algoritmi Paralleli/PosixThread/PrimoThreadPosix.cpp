b9abae261f675d6dd18a27a0ac999e4e538fb72a
Samuu00 <148590518+Samuu00@users.noreply.github.com>
2025-04-09 17:22:52 +0200

    Add files via upload

--- Algoritmi Paralleli/PosixThread/PrimoThreadPosix.cpp
+++ Algoritmi Paralleli/PosixThread/PrimoThreadPosix.cpp
@@ -2,7 +2,7 @@
 #include <pthread.h>
 using namespace std;
 
-void run(void* arg) {
+void* run(void* arg) {
     cout << "Hello from thread!" << endl;
     return NULL;
 }
@@ -11,7 +11,7 @@ int main() {
     pthread_t thread;
     //Creazione del thread, il secondo argomento è NULL perchè non ci sono parametri da passare alla funzione run
     //Il terzo argomento è la funzione da eseguire, il quarto è NULL perchè run ritorna void* e non ha bisogno di argomenti
-    pthread_create(&thread, NULL, run, NULL);
+    pthread_create(&thread, NULL, &run, NULL);
     //Parameter: idThread, puntatore a puntatore del ritorno della funzione;
     pthread_join(thread, NULL);
     //Il join aspetta che il thread finisca prima di continuare l'esecuzione del main
