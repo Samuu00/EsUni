5d891889e1cbd80d7a059e398fb7ee8d98480607
Samuu00 <148590518+Samuu00@users.noreply.github.com>
2025-04-09 17:21:43 +0200

    Update 2Thread.cpp

--- Algoritmi Paralleli/PosixThread/2Thread.cpp
+++ Algoritmi Paralleli/PosixThread/2Thread.cpp
@@ -1,24 +1,26 @@
 #include <iostream>
 #include <pthread.h>
 #include <vector>
+#include <unistd.h> 
 using namespace std;
 
-int nTreads = 2;
+int nThreads = 100;
 
-void run(void* arg) {
+void* run(void* arg) {
     sleep(1);
     int i = *(int*)arg;
-    printf( %d!"\n", i);
+    printf("%d\n", i);
+    delete (int*)arg; 
     return NULL;
 }
 
 int main() {
-    vector<pthread_t> threads(nTreads);
-    for(int i = 0; i < nTreads; i++) {
+    vector<pthread_t> threads(nThreads);
+    for (int i = 0; i < nThreads; i++) {
         int* p = new int(i);
-        pthread_create(&threads[i], NULL, run, p);
+        pthread_create(&threads[i], NULL, &run, (void*)p); 
     }
-    for(int i  = 0; i < nTreads; i++) {
+    for (int i = 0; i < nThreads; i++) {
         pthread_join(threads[i], NULL);
     }
     return 0;
