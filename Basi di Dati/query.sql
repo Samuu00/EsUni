d4ede2d74753f505f86d1f257451816a2ae58a0b
Samuu00 <148590518+Samuu00@users.noreply.github.com>
2025-05-27 23:39:43 +0200

    Add files via upload

--- Basi di Dati/query.sql
+++ Basi di Dati/query.sql
@@ -1,3 +1,8 @@
 SELECT nome FROM clienti
 JOIN ordini ON clienti.id = ordini.id_cliente 
 GROUP BY clienti.nome HAVING COUNT(*) >= 3;
+
+
+SELECT nome FROM clienti
+JOIN ordini ON clienti.id = ordini.id_cliente
+GROUP BY clienti.nome HAVING SUM(ordini.tot_ordine) > (SELECT AVG(tot_ordine) FROM ordini);
