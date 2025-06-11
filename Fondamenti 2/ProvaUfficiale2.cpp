66013c30da7d6fd47504a45ec3c6e55dba0ee429
Samuu00 <148590518+Samuu00@users.noreply.github.com>
2025-06-11 22:45:19 +0200

    Add files via upload

--- Fondamenti 2/ProvaUfficiale2.cpp
+++ Fondamenti 2/ProvaUfficiale2.cpp
@@ -5,48 +5,49 @@
 using namespace std;
 
 //es1
-class codaParrucchiere{
+class codaParrucchiere {
     private:
-        vector<pair<string, queue<string>>> coda; 
-    
+        vector<pair<string, vector<string>>> coda;
+
     public:
-        codaParrucchiere(vector<string>& listaParrucchiere){
-            for(int i = 0; i < listaParrucchiere.size(); i++){
-                coda.push_back(make_pair(listaParrucchiere[i], queue<string>()));
+        codaParrucchiere(vector<string>& listaParrucchiere) {
+            for (string& nome : listaParrucchiere) {
+                coda.push_back({nome, {}});
             }
         }
-        int parrConMenoClienti(){
+
+        int parrConMenoClienti() {
             int min = INT_MAX;
             int pos = -1;
-            for(int i = 0; i < coda.size(); i++){
-                if(coda[i].second.size() < min){
+            for (int i = 0; i < coda.size(); i++) {
+                if (coda[i].second.size() < min) {
                     min = coda[i].second.size();
                     pos = i;
                 }
             }
             return pos;
         }
-        bool aggiungiPrenotazione(string& cliente, string& parrucchiere){
-            for(auto& parr : coda){
-                if(parr.first == parrucchiere){
-                    if(parr.second.size() >= 10){
+
+        bool aggiungiPrenotazione(string& cliente, string& parrucchiere) {
+            for (auto& parr : coda) {
+                if (parr.first == parrucchiere) {
+                    if (parr.second.size() >= 10) {
                         int pos = parrConMenoClienti();
-                        coda[pos].second.push(cliente);
-                        return true;
-                    }else{
-                        parr.second.push(cliente);
-                        return true;
+                        coda[pos].second.push_back(cliente);
+                    } else {
+                        parr.second.push_back(cliente);
                     }
+                    return true;
                 }
             }
             return false;
         }
 
-        bool rimuoviPrenotazione(string& cliente){
-            for(auto& parr : coda){
+        bool rimuoviPrenotazione(string& cliente) {
+            for (auto& parr : coda) {
                 int cont = 0;
-                for(auto& it = parr.second.begin(); it != parr.second.end(); it++){
-                    if(*it == cliente && cont > 3) {
+                for (auto it = parr.second.begin(); it != parr.second.end(); ++it) {
+                    if (*it == cliente && cont > 3) {
                         parr.second.erase(it);
                         return true;
                     }
@@ -55,64 +56,63 @@ class codaParrucchiere{
             }
             return false;
         }
-           
 
-        int tempoAttesa(string& cliente){
-            for(auto& parr : coda){
+        int tempoAttesa(string& cliente) {
+            for (auto& parr : coda) {
                 int tot = 0;
-                for(string& c : parr.second){
+                for (string& c : parr.second) {
                     tot += 15;
-                    if(c == cliente) return tot;
+                    if (c == cliente) return tot;
                 }
             }
             return -1;
         }
 };
-class codaParrucchiereAccurata{
-    private:   
-        vector<pair<string, queue<string>>> coda; 
+class codaParrucchiereAccurata {
+    private:
+        vector<pair<string, vector<string>>> coda;
         map<string, unsigned> tempoAtt;
 
     public:
-        codaParrucchiereAccurata(map<string, unsigned> tempi, vector<string>& listaParrucchiere){
+        codaParrucchiereAccurata(map<string, unsigned> tempi, vector<string>& listaParrucchiere) {
             tempoAtt = tempi;
-            for(int i = 0; i < listaParrucchiere.size(); i++){
-                coda.push_back(make_pair(listaParrucchiere[i], queue<string>()));
+            for (string& nome : listaParrucchiere) {
+                coda.push_back({nome, {}});
             }
         }
 
-        int parrConMenoClienti(){
+        int parrConMenoClienti() {
             int min = INT_MAX;
             int pos = -1;
-            for(int i = 0; i < coda.size(); i++){
-                if(coda[i].second.size() < min){
+            for (int i = 0; i < coda.size(); i++) {
+                if (coda[i].second.size() < min) {
                     min = coda[i].second.size();
                     pos = i;
                 }
             }
             return pos;
         }
-        bool aggiungiPrenotazione(string& cliente, string& parrucchiere){
-            for(auto& parr : coda){
-                if(parr.first == parrucchiere){
-                    if(parr.second.size() >= 10){
+
+        bool aggiungiPrenotazione(string& cliente, string& parrucchiere) {
+            for (auto& parr : coda) {
+                if (parr.first == parrucchiere) {
+                    if (parr.second.size() >= 10) {
                         int pos = parrConMenoClienti();
-                        coda[pos].second.push(cliente);
-                        return true;
-                    }else{
-                        parr.second.push(cliente);
-                        return true;
+                        coda[pos].second.push_back(cliente);
+                    } else {
+                        parr.second.push_back(cliente);
                     }
+                    return true;
                 }
             }
             return false;
         }
 
-        bool rimuoviPrenotazione(string& cliente){
-            for(auto& parr : coda){
+        bool rimuoviPrenotazione(string& cliente) {
+            for (auto& parr : coda) {
                 int cont = 0;
-                for(auto& it = parr.second.begin(); it != parr.second.end(); it++){
-                    if(*it == cliente && cont > 3) {
+                for (auto it = parr.second.begin(); it != parr.second.end(); ++it) {
+                    if (*it == cliente && cont > 3) {
                         parr.second.erase(it);
                         return true;
                     }
@@ -122,12 +122,12 @@ class codaParrucchiereAccurata{
             return false;
         }
 
-        int tempoAttesaAccurato(string& cliente){
-            for(auto& parr : coda){
+        int tempoAttesaAccurato(string& cliente) {
+            for (auto& parr : coda) {
                 int tot = 0;
-                for(string & c : parr.second){
+                for (string& c : parr.second) {
                     tot += tempoAtt[parr.first];
-                    if(c == cliente) return tot;
+                    if (c == cliente) return tot;
                 }
             }
             return -1;
@@ -151,7 +151,7 @@ class Grafo{
             grafo[u].push_back(v);
         }
 
-        int camminoMinimo(Grafo grafo, int p, int a, vector<bool>& visitato, int camm){
+        int camminoMinimo(int p, int a, vector<bool>& visitato, int camm){
             if(p == a) return camm;
             visitato[p] = true;
             int minim = INT_MAX;
@@ -178,14 +178,14 @@ class Grafo{
             return nuovoGrafo;
         }
 
-        bool f(Grafo grafo, int p, int a, pair<unsigned, unsigned> arco){
-            Grafo nuovoGrafo = nuovoGrafo(arco);
+        bool f(int p, int a, pair<unsigned, unsigned> arco){
+            Grafo newGrafo = nuovoGrafo(arco);
             vector<bool> visitato(n, false);
             int camm = 0;
-            int minim = camminoMinimo(grafo, p, a, visitato, camm);
+            int minim = camminoMinimo(p, a, visitato, camm);
             visitato.assign(n, false);
             camm = 0;
-            int newMinim = camminoMinimo(nuovoGrafo, p, a, visitato, camm);
+            int newMinim = newGrafo.camminoMinimo(p, a, visitato, camm);
 
             return newMinim < minim;
         }
