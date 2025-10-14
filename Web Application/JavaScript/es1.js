c82206a4899f47b040f8186cb903a5263ec15462
Samuu00 <148590518+Samuu00@users.noreply.github.com>
2025-10-14 23:15:08 +0200

    Add files via upload

---
+++ Web Application/JavaScript/es1.js
@@ -0,0 +1,8 @@
+s = 20;
+t = "20";
+
+if(s === t){
+    console.log(s, t);
+}else{
+    console.log("Error")
+}
---
+++ Web Application/TypeScript/es1.ts
@@ -0,0 +1,6 @@
+function somma(a: number, b: number): number {
+    return a + b;
+}
+
+const risultato = somma(5, 7);
+console.log(`La somma è: ${risultato}`);
