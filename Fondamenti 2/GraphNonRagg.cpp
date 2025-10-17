22f531e8fcc05758c02388a1b5da2bd86b365b47
Samuu00 <148590518+Samuu00@users.noreply.github.com>
2025-10-17 23:50:10 +0200

    Add files via upload

---
+++ Fondamenti 2/GraphNonRagg.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+class Grafo {
+    private:
+        int numNodi;
+        vector<vector<int>> adj;
+
+    public:
+        Grafo(int n) : numNodi(n), adj(n) {}
+
+        int n() const {
+            return numNodi;
+        }
+
+        void aggiungiArco(int i, int j) {
+            adj[i].push_back(j);
+        }
+
+        void dfs(int u, vector<bool>& visitati, vector<vector<int>>& grafo) {
+            visitati[u] = true;
+            for (int v : grafo[u]) {
+                if (!visitati[v]) {
+                    dfs(v, visitati, grafo);
+                }
+            }
+        }
+
+        int esercizio3() {
+            vector<int> ragg(n(), 0);
+            vector<vector<int>> gT(n());
+
+            for(int i = 0; i < n(); i++){
+                for(int v : adj[i]){
+                    gT[v].push_back(i);
+                }
+            }
+
+            for(int i = 0; i < n(); i++){
+                vector<bool> visitati(n(), false);
+
+                dfs(i, visitati, gT);
+                
+                int cont = 0;
+                for(int j = 0; j < n(); j++){
+                    if(visitati[j]) cont++;
+                }
+                ragg[i] = count;
+            }
+
+            int mNode = INT_MAX;
+            int node = -1;
+            for(int i = 0; i < n(); i++){
+                if(ragg[i] < mNode){
+                    mNode = ragg[i];
+                    node = i;
+                }
+                if(ragg[i] == mNode){
+                    node = min(node, i);
+                }
+            }
+            return node;
+        }
+
+};
---
+++ Fondamenti 2/InvertBinaryTree.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+enum Direzione { SIN, DES };
+class AlberoB {
+    private:
+        int info;  
+        AlberoB* p;          
+        AlberoB* sx;          
+        AlberoB* dx;                 
+
+    public:
+        
+        AlberoB(int valore) : info(valore),  p(nullptr), sx(nullptr), dx(nullptr) {}
+
+        ~AlberoB() {
+            delete sx; 
+            delete dx;
+        }
+
+        AlberoB* figlio(Direzione dir) {
+            if (dir == SIN) {
+                return this->sx;
+            }
+            return this->dx;
+        }
+
+        AlberoB* padre() {
+            return this->p;
+        }
+
+        int radice() {
+            return this->info;
+        }
+
+        bool foglia() {
+            return (this->sx == nullptr && this->dx == nullptr);
+        }
+
+        static bool nullo(AlberoB* a) {
+            return a == nullptr;
+        }
+
+        void invertBinaryTree(AlberoB* a){
+            if(a.nullo()) return;
+
+            swap(a.figlio(SIN), a.figlio(DES));
+
+            invertBinaryTree(a.figlio(SIN));
+            invertBinaryTree(a.figlio(DES));
+        }
+};
