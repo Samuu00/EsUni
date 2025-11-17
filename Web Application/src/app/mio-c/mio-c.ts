53605d14c25137544df1279a8c5c3da2ec53f904
Samuu00 <148590518+Samuu00@users.noreply.github.com>
2025-11-17 23:41:02 +0100

    Add files via upload

---
+++ Web Application/src/app/mio-c/mio-c.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mio-c',
+  imports: [],
+  templateUrl: './mio-c.html',
+  styleUrl: './mio-c.css',
+})
+export class MioC {
+  nome : string = "marco";
+  eta : number = 15;
+  contatore : number = 0;
+}
